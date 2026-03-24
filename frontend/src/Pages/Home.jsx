7d62e329ccd438d0d8b0250bd1f4ae7654c2d06e
Nitinsharma200610 <nitinsharma2006abs@gmail.com>
2026-03-24 22:14:05 +0530

    s

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,55 +1,51 @@
-
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../Pages/Home.css";
-import "../Pages/Cards.jsx";
 import Cards from "../Pages/Cards.jsx";
 import Membership from "./Membership.jsx";
 import Contact from "./Contact.jsx";
 import About from "./About.jsx";
 
 const Home = () => {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const user = localStorage.getItem("user");
+    if (user) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   return (
     <>
-  
-    <div>{/* HERO SECTION */}
-      <section className="home" id="home">
-        <div className="overlay"></div>
+      <div>
+        {/* HERO SECTION */}
+        <section className="home" id="home">
+          <div className="overlay"></div>
 
-        <div className="home-container">
-          {/* LEFT CONTENT */}
-          <div className="home-content">
+          <div className="home-container">
+            <div className="home-content">
 
+              <p>
+                Transform your body. Build raw strength. Burn fat.
+                Train with elite coaches and modern programs.
+              </p>
 
-            <p>
-              Transform your body. Build raw strength. Burn fat.
-              Train with elite coaches and modern programs.
-            </p>
+              <a href={isLoggedIn ? "#" : "/signup"} className="home-btn">
+                {isLoggedIn ? "Joined" : "Join Now"}
+              </a>
 
-
-            <a href="/signup" className="home-btn">
-              Join Now
-            </a>
+            </div>
           </div>
+        </section>
 
+        <Cards />
+      </div>
 
-        </div>
-      </section>
-      <Cards /></div>
-
-      {/*  membership */}
-      <Membership/>
-
-
-      {/* About */}
-      <About/>
-
-
-      {/*  contact */}
-    
-
-        <Contact/>
-  </>
-
+      <Membership />
+      <About />
+      <Contact />
+    </>
   );
 };
 
